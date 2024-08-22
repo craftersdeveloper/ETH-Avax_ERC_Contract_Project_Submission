53b5e95a05ce796431cf5355c10b9168bf4b152a
craftersdeveloper <raghavprabhkar@gmail.com>
2024-08-22 16:26:40 +0530

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -1,1 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Custom_ERC20 is ERC20 {
+
+    constructor(string memory _Token_Name, string memory _Token_Symbol, uint256 initialSupply) ERC20(_Token_Name, _Token_Symbol) {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address _to, uint256 _amount) public {
+        _mint(_to, _amount);
+    }
+
+    function burn(uint256 _amount) public {
+        _burn(msg.sender, _amount);
+    }
+
+    // Custom transfer function
+    function customTransfer(address _to, uint256 _amount) public returns (bool) {
+        require(_to != address(0),"Invalid Recevier Address");
+        
+        // Call the inherited transfer function
+        _transfer(msg.sender, _to, _amount);
+        return true;
+    }
+}
